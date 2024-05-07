39170cf610873f53fbbbfc82710fb046b2dbe1d6
김혜지 <161557112+Hyeji1364@users.noreply.github.com>
2024-05-07 16:36:40 +0900

    Update pychart_I_concert10.py

--- pychart_I_concert10.py
+++ pychart_I_concert10.py
@@ -12,7 +12,7 @@ from datetime import datetime
 
 # 현재 날짜 가져오기
 current_date = datetime.now().strftime("%Y-%m-%d")
-filename = f"chart_I_concert10_{current_date}.json"
+filename = f"interparkconcert/pychart_I_concert10{current_date}.json"
 
 # 웹드라이버 설정
 options = ChromeOptions()
