b602bbc4ef902a09fb1934354cc5fd879a57b3f1
김혜지 <161557112+Hyeji1364@users.noreply.github.com>
2024-05-07 17:00:52 +0900

    Update pychart_I_concert10.py

--- pychart_I_concert10.py
+++ pychart_I_concert10.py
@@ -1,4 +1,4 @@
-import json
+mport json
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
@@ -14,19 +14,21 @@ from datetime import datetime
 current_date = datetime.now().strftime("%Y-%m-%d")
 filename = f"interparkconcert/pychart_I_concert10{current_date}.json"
 
+# WebDriverManager로 ChromeDriver 설치 및 설정
+webdriver_manager = ChromeDriverManager()
+webdriver_manager.install()
+webdriver_path = webdriver_manager.driver
+
 # 웹드라이버 설정
 options = ChromeOptions()
 options.add_argument("--headless")
-browser = webdriver.Chrome(options=options)
+browser = webdriver.Chrome(options=options)  # ChromeOptions를 전달합니다.
 browser.get("https://tickets.interpark.com/contents/ranking")
-
-# RadioButton_wrap__761f0 클래스를 가진 div 요소를 찾기
-search_box = browser.find_element(By.CLASS_NAME, "RadioButton_wrap__761f0")
 
 # "콘서트" 탭 버튼을 찾아서 클릭하기
 try:
     concert_tab_button = WebDriverWait(browser, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//button[text()='콘서트']"))
+        EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), '콘서트')]"))
     )
     concert_tab_button.click()
     print("Clicked '콘서트' tab.")
@@ -36,7 +38,7 @@ except Exception as e:
 
 # "월간" 탭 버튼을 찾아서 클릭하기
 try:
-    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[text()='월간']"))).click()
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), '월간')]"))).click()
     print("Clicked '월간' tab.")
     time.sleep(3)
 except Exception as e:
@@ -45,17 +47,17 @@ except Exception as e:
 page_source = browser.page_source
 soup = BeautifulSoup(page_source, 'html.parser')
 
-# 순위 항목에 대한 상위 컨테이너 찾기
+# Find the parent container for ranking items
 ranking_container = soup.find('div', class_='responsive-ranking-list_rankingListWrap__GM0yK')
 
 concerts = []
 
-# 1-3위 콘서트 순위 정보 추출
+# 1-3위 데이터 추출
 for ranking_item in ranking_container.find_all('div', class_='responsive-ranking-list_rankingItem__PuQPJ'):
     rank = ranking_item.find('div', class_='RankingBadge_badgeNumber__84aeb').text.strip()
     concert_name = ranking_item.find('li', class_='responsive-ranking-list_goodsName__aHHGY').text.strip()
     venue = ranking_item.find('li', class_='responsive-ranking-list_placeName__9HN2O').text.strip()
-    image_url = ranking_item.find('img')['src']
+    image_url = ranking_item.find('img')['src']  # Extracting image URL
 
     concert_data = {
         'Rank': rank,
