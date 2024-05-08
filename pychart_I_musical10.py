c7f1c525514f818a8bc45596600ddbf2c19ca5af
김혜지 <161557112+Hyeji1364@users.noreply.github.com>
2024-05-08 17:46:15 +0900

    Update pychart_I_musical10.py

--- pychart_I_musical10.py
+++ pychart_I_musical10.py
@@ -14,23 +14,22 @@ from datetime import datetime
 current_date = datetime.now().strftime("%Y-%m-%d")
 filename = f"interparkmusical/pychart_I_musical10{current_date}.json"
 
-# WebDriverManager로 ChromeDriver 설치 및 설정
-webdriver_manager = ChromeDriverManager()
-webdriver_manager.install()
-webdriver_path = webdriver_manager.driver
-
 # 웹드라이버 설정
 options = ChromeOptions()
 options.add_argument("--headless")
-browser = webdriver.Chrome(options=options)  # ChromeOptions를 전달합니다.
+service = ChromeService(executable_path=ChromeDriverManager().install())
+browser = webdriver.Chrome(service=service, options=options)
 browser.get("https://tickets.interpark.com/contents/ranking")
 
-# "뮤지컬" 탭 버튼을 찾아서 클릭하기
+# RadioButton_wrap__761f0 클래스를 가진 div 요소를 찾기
+search_box = browser.find_element(By.CLASS_NAME, "RadioButton_wrap__761f0")
+
+# "콘서트" 탭 버튼을 찾아서 클릭하기
 try:
-    musical_tab_button = WebDriverWait(browser, 10).until(
+    concert_tab_button = WebDriverWait(browser, 10).until(
         EC.element_to_be_clickable((By.XPATH, "//button[text()='뮤지컬']"))
     )
-    musical_tab_button.click()
+    concert_tab_button.click()
     print("Clicked '뮤지컬' tab.")
     time.sleep(3)  # 페이지가 완전히 로드될 때까지 대기
 except Exception as e:
@@ -55,38 +54,38 @@ concerts = []
 # 1-3위 데이터 추출
 for ranking_item in ranking_container.find_all('div', class_='responsive-ranking-list_rankingItem__PuQPJ'):
     rank = ranking_item.find('div', class_='RankingBadge_badgeNumber__84aeb').text.strip()
-    musical_name = ranking_item.find('li', class_='responsive-ranking-list_goodsName__aHHGY').text.strip()
+    concert_name = ranking_item.find('li', class_='responsive-ranking-list_goodsName__aHHGY').text.strip()
     venue = ranking_item.find('li', class_='responsive-ranking-list_placeName__9HN2O').text.strip()
     image_url = ranking_item.find('img')['src']  # Extracting image URL
 
-    musical_data = {
+    concert_data = {
         'Rank': rank,
-        'musicalName': musical_name,
+        'ConcertName': concert_name,
         'Venue': venue,
         'ImageURL': image_url
     }
-    musicals.append(musical_data)
+    concerts.append(concert_data)
 
-# 4-10위 뮤지컬 순위 정보 추출
+# 4-10위 콘서트 순위 정보 추출
 rank_list_4_to_10 = soup.find_all('div', class_='responsive-ranking-list_rankingItem__PuQPJ')[3:10]  # 4위부터 10위까지의 항목 추출
 for ranking_item in rank_list_4_to_10:
     rank = ranking_item.find('div', class_='RankingBadge_badgeNumberColor__d45a0').text.strip()
-    musical_name = ranking_item.find('li', class_='responsive-ranking-list_goodsName__aHHGY').text.strip()
+    concert_name = ranking_item.find('li', class_='responsive-ranking-list_goodsName__aHHGY').text.strip()
     venue = ranking_item.find('li', class_='responsive-ranking-list_placeName__9HN2O').text.strip()
     image_url = ranking_item.find('img')['src']  # Extracting image URL
 
-    musical_data = {
+    concert_data = {
         'Rank': rank,
-        'musicalName': musical_name,
+        'ConcertName': concert_name,
         'Venue': venue,
         'ImageURL': image_url
     }
-    musicals.append(musical_data)
+    concerts.append(concert_data)
 
 # json파일로 저장
 with open(filename, 'w', encoding='utf-8') as file:
-    json.dump(musicals, file, ensure_ascii=False, indent=4)
+    json.dump(concerts, file, ensure_ascii=False, indent=4)
 
 # 출력
-for musical_data in musicals:
-    print(musical_data)
+for concert_data in concerts:
+    print(concert_data)
