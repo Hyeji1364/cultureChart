15922147da4e7332128dfe7a1874c87951a76cac
김혜지 <161557112+Hyeji1364@users.noreply.github.com>
2024-05-31 14:38:07 +0900

    Update pychart_Y_exhibiton10.py

--- pychart_Y_exhibiton10.py
+++ pychart_Y_exhibiton10.py
@@ -14,14 +14,12 @@ from datetime import datetime
 current_date = datetime.now().strftime("%Y-%m-%d")
 filename = f"yes24exhibiton/pychart_Y_exhibiton10{current_date}.json"
 
-# 웹드라이버 설정
+# 웹드라이버 설치
 options = ChromeOptions()
 options.add_argument("--headless")
 browser = webdriver.Chrome(options=options)
-
-# 웹 사이트 접속
 browser.get("http://ticket.yes24.com/Rank/All")
-time.sleep(2)  # 페이지 로딩 대기
+time.sleep(5)  # 페이지 로딩 대기
 
 # 전시/행사 카테고리로 이동
 event_link = WebDriverWait(browser, 10).until(
@@ -35,7 +33,7 @@ monthly_category = WebDriverWait(browser, 10).until(
     EC.presence_of_element_located((By.XPATH, "//a[contains(@categoryid, '3') and contains(text(), '월간')]"))
 )
 monthly_category.click()
-time.sleep(2)  # 월간 카테고리 로딩 대기
+time.sleep(5)  # 월간 카테고리 로딩 대기
 
 # 웹 페이지 소스 가져오기
 page_source = browser.page_source
@@ -44,7 +42,7 @@ soup = BeautifulSoup(page_source, 'html.parser')
 # 정보 추출
 events_data = []
 
-# Extracting data from rank-best div
+# 1-3위 전시/행사 데이터 추출
 rank_best_div = soup.find('div', class_='rank-best')
 if rank_best_div:
     event_divs = rank_best_div.find_all('div')
@@ -58,20 +56,20 @@ if rank_best_div:
             event_info['rank'] = event_link.find('p', class_='rank-best-number').find('span').get_text(strip=True)
             events_data.append(event_info)
 
-# 전시/행사 순위 정보 추출
-rank_list = soup.find('div', class_='rank-list')  # 첫번째 rank-list 컨테이너 선택
-if rank_list:
-    items = rank_list.find_all('div', recursive=False)[:7]  # 4위부터 10위까지의 정보 추출
+# 전체 전시/행사 순위 정보 추출
+rank_lists = soup.find_all('div', class_='rank-list')  # 모든 rank-list 컨테이너 선택
+for rank_list in rank_lists:
+    items = rank_list.find_all('div', recursive=False)  # 모든 항목 추출
     for item in items:
         event_info = {}
         title_link = item.find('p', class_='rank-list-tit').find('a')
         image = item.find('img', class_='rank-list-img')
         date_location = item.find_all('p')[-1]
-        fluctuation_div = item.find('div', class_='fluctuation')
+        fluctuation_div = item.find('div', class_='fluctuation')  # 순위 정보를 포함하는 div 태그를 찾는다.
 
         # 순위 정보를 추출
         if fluctuation_div:
-            rank_span = fluctuation_div.find('p').find('span')
+            rank_span = fluctuation_div.find('p').find('span')  # 첫 번째 <p> 태그 내의 <span>에서 순위를 찾는다.
             rank = rank_span.text.strip() if rank_span else 'No rank provided'
         else:
             rank = 'No rank provided'
@@ -82,7 +80,7 @@ if rank_list:
         event_info['rank'] = rank
         events_data.append(event_info)
 
-# 결과를 JSON 파일로 저장
+# JSON 파일로 저장
 with open(filename, 'w', encoding='utf-8') as file:
     json.dump(events_data, file, ensure_ascii=False, indent=4)
 
