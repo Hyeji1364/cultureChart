b2e7b571f5ac2eba6de50930d01e6bc17fd7b751
김혜지 <161557112+Hyeji1364@users.noreply.github.com>
2024-06-13 16:25:11 +0900

    Update pychart_Y_concert10.py

--- pychart_Y_concert10.py
+++ pychart_Y_concert10.py
@@ -54,7 +54,7 @@ if rank_best_div:
             concert_info['ImageURL'] = concert_link.find('img')['src']
             concert_info['Venue'] = concert_link.find('p', class_='rlb-sub-tit').get_text(strip=True)
             concert_info['rank'] = concert_link.find('p', class_='rank-best-number').find('span').get_text(strip=True)
-            concert_info['site'] = "http://ticket.yes24.com" + concert_link['href']
+            concert_info['site'] = "http://ticket.yes24.com/Rank/All"  # 공통 URL로 설정
             change_status = concert_link.find('span', class_='rank-best-number-new')
             if change_status:
                 concert_info['change'] = 'NEW'
@@ -106,7 +106,7 @@ for rank_list in rank_lists:
         concert_info['Venue'] = date_location.get_text(strip=True) if date_location else 'No date and location provided'
         concert_info['rank'] = rank
         concert_info['change'] = change
-        concert_info['site'] = "http://ticket.yes24.com" + title_link['href'] if title_link else 'No site provided'
+        concert_info['site'] = "http://ticket.yes24.com/Rank/All"  # 공통 URL로 설정
         concerts_data.append(concert_info)
 
 # JSON 파일로 저장
